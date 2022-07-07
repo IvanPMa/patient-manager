3941789583675a66c4d71c8f10ad01214094933f
IvanPMa <pachecomaldonadoivan@gmail.com>
2022-07-06 19:31:29 -0500

    First

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,28 @@
+const  Formulario = ()=> {
+
+    return (
+        <div className="md:w-1/2 lg:w-2/5">
+            <h2 className="font-black text-3xl text-center">
+                Seguimiento Pacientes
+            </h2>
+
+            <p className="text-lg mt-5 text-center">
+                Añade Pacientes y {''}
+                <span className="text-indigo-600 font-bold">Administralos</span>
+            </p>
+
+            <form className="bg-white">
+                <div>
+                    <label>Nombre Mascota</label>
+
+                    <input 
+                        type="text" 
+                        placeholder="Nombre de la Mascota"
+                    />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Formulario;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+const ListadoPacientes = () =>{
+
+    return (
+        <div className="w-1/2 lg:w-3/5">
+            <h2>Listado de Pacientes</h2>
+        </div>
+    )
+
+}
+
+export default ListadoPacientes;
